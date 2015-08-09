980a2ed16ff80dee908f9b147223f7822955d479
Matthew Keas <matthiasak@gmail.com>
2015-08-09 13:46:25 -0500

    initial release 0.0.1

---
+++ resolver.js
@@ -0,0 +1,87 @@
+import m from './mithril'
+
+const containsAllProps = (obj) => Object.keys(obj).reduce((a,name) => a && obj[name] instanceof Function && obj[name].name ==='prop', true)
+
+export const container = (component, resolve = {}) =>
+    (resolver) =>
+        m.component({
+            controller: function() {
+                // run the controller on this controller,
+                // then return that value
+                // if the value is undefined, mithril passes `this`
+                // to the `view()` as `ctrl`
+                m.startComputation()
+                resolver.resolve(resolve).then((_resolve) => {
+                    Object.assign(resolve, _resolve)
+                    m.endComputation()
+                })
+                return component.controller.call(this)
+            },
+            view: function(ctrl, args) {
+                if(!containsAllProps(resolve)) return
+                return m.component(component, {...args, ...resolve, resolver})
+            }
+        })
+
+let _resolver  = (states = {}) => {
+    let promises = []
+
+    const _await = (_promises = []) => {
+        promises = promises.concat(_promises)
+        return Promise.all(promises)
+    }
+
+    const finish = () => {
+        const total = promises.length
+        return Promise.all(promises).then(values => {
+            if(promises.length > total){
+                return finish()
+            }
+            return values
+        })
+    }
+
+    const resolve = (props) => {
+        const keys = Object.keys(props)
+        if (!keys.length) {
+            return Promise.resolve(true)
+        }
+
+        let f = []
+        const promises = keys.reduce((a, name) => {
+            let p = m.prop()
+            if(props[name] instanceof Function && props[name]().then){
+                f.push(props[name]().then(p))
+                a[name] = p
+            }
+            return a
+        }, {})
+
+        return _await(f).then(() => promises)
+    }
+
+    return {
+        finish,
+        resolve
+    }
+}
+
+_resolver.renderToString = (component, renderer, instance = resolver()) => {
+    const t = component(instance)
+    renderer(t)
+    return instance.finish().then(() => {
+        // instance.freeze()
+        return renderer(t)
+    })
+}
+
+_resolver.render = (component, node, instance = resolver()) => {
+    const t = component(instance)
+    m.render(node, t)
+    return instance.finish().then(() => {
+        // instance.freeze()
+        m.mount(node, t)
+    })
+}
+
+export const resolver = _resolver
